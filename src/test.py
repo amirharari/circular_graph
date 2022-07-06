1e571094bb382a24045594806692da1b280043a1
Amir-Mano <amirmano14@gmail.com>
2022-07-06 16:01:33 +0300

    updated test

--- src/test.py
+++ src/test.py
@@ -2,6 +2,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import nxviz as nv
 import data_loader
+import nxviz as nz
+
 
 a = data_loader.load_data(
 '../data/examples/connectivity_matrices/atlas-brainnetome_count-1M_scale-length_connectome.csv',
@@ -10,4 +12,3 @@ a = data_loader.load_data(
 "Label",
 "ROIname")
 plt.show()
-val = input("Enter your value: ")
