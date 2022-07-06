27425a4f95eb17e6ca7c5f4b6bc0826d0db115cc
Amir-Mano <amirmano14@gmail.com>
2022-07-06 16:04:10 +0300

    updated test

--- src/test.py
+++ src/test.py
@@ -5,10 +5,16 @@ import data_loader
 import nxviz as nz
 
 
-a = data_loader.load_data(
-'../data/examples/connectivity_matrices/atlas-brainnetome_count-1M_scale-length_connectome.csv',
-'../data/examples/parcellation_schemes/BNA_with_cerebellum.csv',
-'Lobe',
-"Label",
-"ROIname")
+normalized_connectivity_matrix, groups = data_loader.load_data(
+    "../data/examples/connectivity_matrices/atlas-brainnetome_count-1M_scale-length_connectome.csv",
+    "../data/examples/parcellation_schemes/BNA_with_cerebellum.csv",
+    "Lobe",
+    "Label",
+    "ROIname",
+)
+Threshold = 0.5
+filtered_matrix = normalized_connectivity_matrix
+filtered_matrix[normalized_connectivity_matrix < Threshold] = 0
+G = nx.from_numpy_array(filtered_matrix)
+nz.CircosPlot(G)
 plt.show()
