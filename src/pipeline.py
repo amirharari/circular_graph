054c1d0673e41019d237fa8b32be9161fccff511
hakerona <ronahannahh@mail.tau.ac.il>
2022-07-07 18:52:05 +0300

    pipeline update

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,7 +4,9 @@ from graph_creator import create_graph
 from normalize_threshold import normalize_threshold
 from data_loader import load_data
 import networkx as nx
+import Circular_graph
 
+"""Run as functions"""
 connectivity_matrix, groups = load_data(
     "data/examples/connectivity_matrices/atlas-brainnetome_count-1M_scale-length_connectome.csv",
     "data/examples/parcellation_schemes/BNA_with_cerebellum.csv",
@@ -14,9 +16,14 @@ connectivity_matrix, groups = load_data(
 filtered_matrix = normalize_threshold(connectivity_matrix, threshold = 0.5)
 graph = create_graph(filtered_matrix,groups)
 
-
-atlas_path = "data/examples/parcellation_schemes/BNA_with_cerebellum.csv"
-conmat_path = "data/examples/connectivity_matrices/atlas-brainnetome_count-1M_scale-length_connectome.csv"
+"""Run as Class: Circular_graph
+Enter the connectivity map of interest.
+Enter the Brain Atlas of interest.
+"""
+conmat = "BNA_with_cerebellum.csv"
+atlas = "atlas-brainnetome_count-1M_scale-length_connectome.csv"
+conmat_path = "data/examples/connectivity_matrices/{}".format(atlas)
+atlas_path = "data/examples/parcellation_schemes/{}".format(conmat)
 
 bna = Circular_graph(conmat_path, atlas_path)
 bna.show_graph()
