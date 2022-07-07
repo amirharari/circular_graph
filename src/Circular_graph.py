ef59a4cd01aa74db2d30b5613bbaf667bfd1cf4f
Amir <amir@ermetic.com>
2022-07-07 19:54:00 +0300

    Added docstring

--- src/Circular_graph.py
+++ src/Circular_graph.py
@@ -223,7 +223,20 @@ class Circular_graph:
         self.filtered_normalize_matrix = filtered_normalize_matrix
 
     def create_graph(self):
+        """
+               This function encapsulate the logic of graph creation
+        """
+
         def rotate_node_by_count(g, count):
+            """
+            This function gets a nx graph and rotate it by count.
+            Parameters
+            ----------
+            g: networkx.Graph
+
+            count: int
+                by how many points the graph should be rotated
+            """
             values = [g.nodes()[node]["sort"] for node in g.nodes]
             print(count)
             values.sort()
@@ -232,8 +245,24 @@ class Circular_graph:
                 if g.nodes()[node]["sort"] in values:
                     g.nodes()[node]["sort"] += -1000
 
-        def add_padding(g, padding_size, sort_value):
-            for i in range(padding_size):
+        def add_padding(g, padding_count, sort_value):
+            """
+            This function gets a nx graph and add empty padding values
+            ----------
+            g: networkx.Graph
+
+            padding_count: int
+                how many empty points should be added
+
+            sort_value: int
+                running index for sorting
+
+            Returns
+            -------
+              sort_value: int
+                running index for sorting
+            """
+            for i in range(padding_count):
                 node_value = i * randint(1000, 100000000)
                 g.add_node(node_value)
                 g.nodes()[node_value]["group"] = "_"
@@ -243,6 +272,25 @@ class Circular_graph:
             return sort_value
 
         def add_values(g, items, sort_value):
+            """
+            This function add values to a graph
+            ----------
+            g: networkx.Graph
+
+            items: dictionary<(int,str>
+                dictionary from key to tuple of int and label
+
+            sort_value: int
+                running index for sorting
+
+            rotate_nodes: Boolean
+                should rotate the list labels
+
+            Returns
+            -------
+              sort_value: int
+                running index for sorting
+            """
             for k1, v1 in items:
                 for i1 in v1:
                     g.nodes()[i1[0]]["group"] = k1
